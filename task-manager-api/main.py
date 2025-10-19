85138798a97754c88dc262d532aa8450528e8a4a
viranarullita <viranarullita3@gmail.com>
2025-10-19 12:46:22 +0700

    initial commit

--- task-manager-api/main.py
+++ task-manager-api/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=["https://task-manager-project-vira.vercel.app/"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
